fc13c9223c5043f6d041f39f5f51d83be54ef503
Athul Cyriac Ajay <athul8720@gmail.com>
2019-12-05 23:07:10 +0530

    Update install_main.sh

--- scripts/install_main.sh
+++ scripts/install_main.sh
@@ -5,3 +5,14 @@ printf "\e[1;34m==> Installing shelby\e[0m\n"
 sudo mv -f shelby /usr/local/bin/shelby
 sudo chmod 775 /usr/local/bin/shelby
 printf "\e[1;32m==> shelby successfully installed 🎉🎉 Enjoy....."
+cat << "EOF"
+ ____    __              ___    __
+/\  _`\ /\ \            /\_ \  /\ \
+\ \,\L\_\ \ \___      __\//\ \ \ \ \____  __  __
+ \/_\__ \\ \  _ `\  /'__`\\ \ \ \ \ '__`\/\ \/\ \
+   /\ \L\ \ \ \ \ \/\  __/ \_\ \_\ \ \L\ \ \ \_\ \
+   \ `\____\ \_\ \_\ \____\/\____\\ \_,__/\/`____ \
+    \/_____/\/_/\/_/\/____/\/____/ \/___/  `/___/> \
+                                              /\___/
+                                              \/__/
+EOF
