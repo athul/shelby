8e4201785fe5925f9d710eaeb08160a1bd719604
Athul Cyriac Ajay <athul8720@gmail.com>
2019-12-06 16:31:23 +0530

    Update install_main.sh

--- scripts/install_main.sh
+++ scripts/install_main.sh
@@ -1,10 +1,11 @@
 # !/bin/bash
-printf "\e[1;34m==> Downloading shelby for $(uname -s)\e[0m\n"
-wget https://github.com/athul/shelby/releases/download/0.1.0/shelby
+OS=$(uname -s)
+printf "\e[1;34m==> Downloading shelby for $(OS)\e[0m\n"
+wget https://github.com/athul/shelby/releases/download/0.1.0/shelby_${OS}_amd64
 printf "\e[1;34m==> Installing shelby\e[0m\n"
 sudo mv -f shelby /usr/local/bin/shelby
 sudo chmod 775 /usr/local/bin/shelby
-printf "\e[1;32m==> shelby successfully installed 🎉🎉 Enjoy....."
+printf "\e[1;32m==> shelby successfully installed 🎉🎉 Enjoy.....\n"
 cat << "EOF"
  ____    __              ___    __
 /\  _`\ /\ \            /\_ \  /\ \
