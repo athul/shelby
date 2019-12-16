2a2f76c0d0f54add602fd0f452b8d3da21b59cfd
athul <athul8720@gmail.com>
2019-12-16 09:29:14 +0530

    Improved download script

--- scripts/install_main.sh
+++ scripts/install_main.sh
@@ -1,8 +1,15 @@
 # !/bin/bash
 OS=$(uname -s)
+get_latest_release() {
+	curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub API
+	grep '"tag_name":' | # Get tag line
+	sed -E 's/.*"v([^"]+)".*/\1/' # Pluck version number
+}
+VERSION="$(get_latest_release 'athul/shelby')"
 printf "\e[1;34m==> Downloading shelby for $(OS)\e[0m\n"
-wget https://github.com/athul/shelby/releases/download/v0.1.5/shelby-${OS}-amd64
+wget  https://github.com/athul/shelby/releases/download/v${VERSION}/shelby-${OS}-amd64
 printf "\e[1;34m==> Installing shelby\e[0m\n"
+mv -f shelby-${OS}-amd64 shelby
 sudo mv -f shelby /usr/local/bin/shelby
 sudo chmod 775 /usr/local/bin/shelby
 printf "\e[1;32m==> shelby successfully installed 🎉🎉 Enjoy.....\n"
