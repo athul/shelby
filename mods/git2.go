f1b89777588f8c9f39ba3c856462e6778f787b0a
athul <athul8720@gmail.com>
2020-01-06 09:52:54 +0530

    Trying on git2go

--- mods/git2.go
+++ mods/git2.go
@@ -1,2 +1,24 @@
 package mods
 
+import (
+	"fmt"
+	"os"
+
+	git "gopkg.in/libgit2/git2go.v27"
+)
+
+func main() {
+	dir, err := os.Getwd()
+	r, err := git.OpenRepository(dir)
+	/* 	ref, err1 := r.Head()
+	   	branch := plumbing.Str
+		   rem, err2 := r.Remotes() */
+	rem, err2 := r.Remotes.Lookup("origin")
+	url := rem.Url()
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Print(r, url, rem, err2)
+}
+
+//func getbranch(path string)
